3510d73b68fe7f9885107ee5c146b11acb099423
Lucas Rosello <l.martin.rosello@gmail.com>
2020-07-06 10:42:58 -0300

    añado las dos funciones en el modulo de autenticacion. Loguear y registrar

--- controllers/autenticacionController.js
+++ controllers/autenticacionController.js
@@ -3,3 +3,16 @@ var router = express.Router();
 const bcrypt = require('bcrypt');
 //var autenticacionModel = require("ruta")
 const jwt = require('jsonwebtoken');
+
+
+module.exports = {
+    registrar: async function(req, res, next){
+        res.status(200).json({status: "success", message: "work in progress, registrar", data: null});
+
+    },
+    loguear: async function(req, res, next){
+        res.status(200).json({status: "success", message: "work in progress, loguear", data: null});
+
+    },
+
+}
