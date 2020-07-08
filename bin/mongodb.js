dcf9cb61c1303ddbb126fdcc882b0c0b5f93a0e6
lucas <l.martin.rosello@gmail.com>
2020-07-08 08:43:47 -0300

    informo cuando la mongo esta apagada

--- bin/mongodb.js
+++ bin/mongodb.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/zyroh', { useNewUrlParser: true, useUnifiedTopology: true }, function(error){
    if(error){
-      throw error; 
+      console.log("\x1b[31mHubo un error conectandose a la base de datos. Asegurese de que la instancia se encuentra levantada.\x1b[0m")
    }else{
       console.log('Conectado a MongoDB');
    }
