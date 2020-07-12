27b46f7c3643abc9b8d5f0429808372e49396d52
lucas <l.martin.rosello@gmail.com>
2020-07-12 17:31:41 -0300

    creo el login de usuario y devuelvo el token}

--- controllers/autenticacionController.js
+++ controllers/autenticacionController.js
@@ -21,7 +21,20 @@ module.exports = {
         }
     },
     loguear: async function(req, res, next){
-        res.status(200).json({status: "success", message: "work in progress, loguear", data: null});
+        var usuario = await autenticacionModel.findOne({usuario: req.body.usuario});
+            if(usuario){
+                if(bcrypt.compareSync(req.body.password, usuario.password)){
+                    const token = jwt.sign({id: usuario._id}, req.app.get('secretKey'), {expiresIn: '1h'});
+                    console.log(token, usuario)
+                    res.status(200).json({status: "success", message: "Usuario encontrado", data: {user: usuario, token: token}});
+                }
+                else{
+                    res.status(500).json({status: "wrong_password", message: "Contraseña incorrecta", data: null});
+                }
+            }
+            else{
+                res.status(404).json({status: "not_found", message: "Usuario no encontrado", data: null});
+            }
 
     },
 
