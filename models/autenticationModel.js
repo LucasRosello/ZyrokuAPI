879bfd5edf0251af2615256182dd6ac3417efc8e
lucas <l.martin.rosello@gmail.com>
2020-10-01 22:04:52 -0300

    cambio usuario por email

--- models/autenticationModel.js
+++ models/autenticationModel.js
@@ -4,15 +4,20 @@ const bcrypt = require('bcrypt');
 
 var UsuarioSchema = mongoose.Schema({
     nombre: String,
-    usuario: {
+    email: {
         type: String,
-        required: true
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'El email es obligatorio',
+        validate: [validateEmail, 'Porfavor ingrese un email valido'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Porfavor ingrese un email valido']
     },
     password:{
         type: String,
         trim: true,
         required: true
-    }
+    },
 })
 
 UsuarioSchema.pre('save', function(next){
